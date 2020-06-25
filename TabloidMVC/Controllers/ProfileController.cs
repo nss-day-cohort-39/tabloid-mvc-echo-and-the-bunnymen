51bb9479958a20c7f20462d20a611662c6174522
calvincurry22 <calvincurry22@gmail.com>
2020-06-25 17:23:46 -0500

    Added feature to allow users to view all user profiles

---
+++ TabloidMVC/Controllers/ProfileController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly PostRepository _postRepository;
+        private readonly CategoryRepository _categoryRepository;
+        private readonly UserProfileRepository _userProfileRepository;
+
+
+        public ProfileController(IConfiguration config)
+        {
+            _postRepository = new PostRepository(config);
+            _categoryRepository = new CategoryRepository(config);
+            _userProfileRepository = new UserProfileRepository(config);
+        }
+        // GET: ProfileController
+        public ActionResult Index()
+        {
+            List<UserProfile> users = _userProfileRepository.GetAllProfiles();
+            return View(users);
+        }
+
+        // GET: ProfileController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProfileController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProfileController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProfileController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProfileController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProfileController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProfileController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
